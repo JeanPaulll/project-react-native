7111a0055ba499dcadedda8fe6ae76160fd2b97b
Jean Paul <jeanpaulwebb@gmail.com>
2019-06-15 20:45:24 -0300

    Inclusão do React-Navigation - npm install react-navigation

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +20,32 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+
+        <View style={styles.postContainer}>
+         <Text style={styles.postTitle}> Titulo do post</Text>
+         <Text style={styles.postDescription}> Descrição de teste, descrição de teste </Text>
+        </View>
+
+        <View style={styles.postContainer}>
+          <Text style={styles.postTitle}> Titulo do post</Text>
+          <Text style={styles.postDescription}> Descrição de teste, descrição de teste </Text>
+        </View>
+
+        <View style={styles.postContainer}>
+          <Text style={styles.postTitle}> Titulo do post</Text>
+          <Text style={styles.postDescription}> Descrição de teste, descrição de teste </Text>
+        </View>
+
+        <View style={styles.postContainer}>
+          <Text style={styles.postTitle}> Titulo do post</Text>
+          <Text style={styles.postDescription}> Descrição de teste, descrição de teste </Text>
+        </View>
+
+      </ScrollView>
     );
   }
 }
@@ -32,14 +53,30 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  postContainer: {
+    padding: 20,
+    marginHorizontal: 10,
+    marginVertical: 2,
+    // width: '90%',
+    backgroundColor: 'green',
+    borderRadius: 5
+  },
+  postTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#F5FCFF'
+  },
+  postDescription: {
+    color: '#FFF',
   },
   instructions: {
     textAlign: 'center',
